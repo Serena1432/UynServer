9b8df9e6325c8ccb468649cc5f63d92c53d78283
Nico Levianth <nambmlc132@gmail.com>
2021-02-16 13:13:37 +0700

    Added the get function the togglequote file

--- ToggleQuote.php
+++ ToggleQuote.php
@@ -1,30 +1,46 @@
 <?php
 if ($_GET["token"] == "3LetaV3Ja94e6wttSJJ26RD5bwVuSp5N") {
-include "Connection.php";
+    include "Connection.php";
 
-$sql = "SELECT * FROM `uyn_toggleping` WHERE server = " . addslashes($_GET["server"]);
-$result = $conn->query($sql);
+    if ($_GET["type"] == "set") {
+        $sql = "SELECT * FROM `uyn_toggleping` WHERE server = " . addslashes($_GET["server"]);
+        $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-while($row = $result->fetch_assoc()) {
-$sql2 = "UPDATE `uyn_toggleping` SET `enabled` = " . addslashes($_GET["enabled"]) . " WHERE server = " . addslashes($_GET["server"]);
-   if (mysqli_query($conn, $sql2)) {
-       echo "Success";
-   } else {
-       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-   }
-}
-}
-else {
-   $sql = "INSERT INTO `uyn_toggleping` (`server`, `enabled`) VALUES (" . addslashes($_GET["server"]) . ", " . addslashes($_GET["enabled"]) . ")";
-   if (mysqli_query($conn, $sql)) {
-       echo "Success";
-   } else {
-       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-   }
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $sql2 = "UPDATE `uyn_toggleping` SET `enabled` = " . addslashes($_GET["enabled"]) . " WHERE server = " . addslashes($_GET["server"]);
+                if (mysqli_query($conn, $sql2)) {
+                    echo "Success";
+                } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+            }
+        }
+        else {
+            $sql = "INSERT INTO `uyn_toggleping` (`server`, `enabled`) VALUES (" . addslashes($_GET["server"]) . ", " . addslashes($_GET["enabled"]) . ")";
+            if (mysqli_query($conn, $sql)) {
+                echo "Success";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+        }
+    }
+    else if ($_GET["type"] == "get") {
+        $sql = "SELECT * FROM `uyn_toggleping` WHERE 1";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $info[$row["server"]] = $row["enabled"];
+            }
+            echo json_encode($info);
+        } else {
+            echo "[]";
+        }
+        $conn->close();
+    }
 }
 $conn->close();
-}
 else {
 echo 'Invalid token';
 }
